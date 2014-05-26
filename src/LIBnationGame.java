dff1d8e9ec1eece44680cbf99871dfd9453801c8
davidmfinol <davidmfinol@gmail.com>
2014-05-26 13:27:17 -0500

    Adding everything to git

---
+++ src/LIBnationGame.java
@@ -0,0 +1,270 @@
+package libnation;
+
+import javax.swing.JApplet;
+import java.applet.AudioClip;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.font.FontRenderContext;
+import java.awt.geom.Rectangle2D;
+
+/**
+ *@(#)LIBnationGame.java
+ *
+ *The Fighting Game
+ *
+ *@author David Finol
+ *@version 1.0 12/17/06
+*/
+public class LIBnationGame extends JApplet implements Runnable
+{
+	private Thread GameRun;//keeps the the game running
+	private final int APPLET_WIDTH = 500;
+	private final int APPLET_HEIGHT = 375;
+	private Image background, backbuffer;//sets the background image
+	private AudioClip music;//background, later coming attack and hurt sounds
+	private Player p1, p2;
+	private String name1 = "Mossflower", name2 = "Nick";//character select
+	private String level = "Arrival_of_Dawn", victory;//causes level select
+	private String healthsign1, healthsign2;//health output
+	private int p1width = 45, p1height = 30, p1atk = 4, p1def = 2, p1speed = 5, p1range = 1, health = 500;
+	private int p2width = 40, p2height = 40, p2atk = 3, p2def = 2, p2speed = 1, p2range = 5, teles = 10;
+	private boolean gravity, offscreen, dissappear, teleport, team, com;//features and options
+	private boolean GameRunning, spec1, spec2, death1, death2;//in-game events
+	private boolean p1moveLeft, p1moveRight, p1jump, p1fall, p1attack, p1tele;//boolean flags
+	private boolean p2moveLeft, p2moveRight, p2jump, p2fall, p2attack, p2tele;//for moving at the same time
+	private Graphics backg;//prevents flickering
+	private Graphics2D Stringmaker;//for health and names
+	private Rectangle2D bounds, bounds2;//to find where to put names & health
+	private Font font = new Font("SansSerif", Font.BOLD+Font.ITALIC, 24);//for health & names
+	private FontRenderContext context;//for outputting names & health
+	private KeyListener Controller;
+/**The control movements for a player
+ */
+	class Controls implements KeyListener
+	{
+/**Unused Method
+ *@param the key typed
+ */
+		public void keyTyped (KeyEvent typed)
+		{
+		}
+/**Controls the characters (starts movement)
+ *@param the key pressed
+ */
+		public void keyPressed (KeyEvent pressed)
+		{
+			if(pressed.getKeyCode() == KeyEvent.VK_A)
+				p1moveLeft = true;
+			if(pressed.getKeyCode() == KeyEvent.VK_W)
+				p1jump = true;
+			if(pressed.getKeyCode() == KeyEvent.VK_D)
+				p1moveRight = true;
+			if(pressed.getKeyCode() == KeyEvent.VK_S)
+				p1fall = true;
+			if(pressed.getKeyCode() == KeyEvent.VK_CAPS_LOCK)
+				p1attack = true;
+			if(pressed.getKeyCode() == KeyEvent.VK_F)
+				p1tele = true;
+			if(pressed.getKeyCode() == KeyEvent.VK_LEFT)
+				p2moveLeft = true;
+			if(pressed.getKeyCode() == KeyEvent.VK_UP)
+				p2jump = true;
+			if(pressed.getKeyCode() == KeyEvent.VK_RIGHT)
+				p2moveRight = true;
+			if(pressed.getKeyCode() == KeyEvent.VK_DOWN)
+				p2fall = true;
+			if(pressed.getKeyCode() == KeyEvent.VK_NUMPAD0)
+				p2attack= true;
+			if(pressed.getKeyCode() == KeyEvent.VK_CONTROL)
+				p2tele = true;
+		}
+/**Causes the the player to stop moving
+ *@param the key released
+ */
+		public void keyReleased (KeyEvent released)
+		{
+			if(released.getKeyCode() == KeyEvent.VK_A)
+				p1moveLeft = false;
+			if(released.getKeyCode() == KeyEvent.VK_W)
+				p1jump = false;
+			if(released.getKeyCode() == KeyEvent.VK_D)
+				p1moveRight = false;
+			if(released.getKeyCode() == KeyEvent.VK_S)
+				p1fall = false;
+			if(released.getKeyCode() == KeyEvent.VK_CAPS_LOCK)
+				p1attack = false;
+			if(released.getKeyCode() == KeyEvent.VK_F)
+				p1tele = false;
+			if(released.getKeyCode() == KeyEvent.VK_LEFT)
+				p2moveLeft = false;
+			if(released.getKeyCode() == KeyEvent.VK_UP)
+				p2jump = false;
+			if(released.getKeyCode() == KeyEvent.VK_RIGHT)
+				p2moveRight = false;
+			if(released.getKeyCode() == KeyEvent.VK_DOWN)
+				p2fall = false;
+			if(released.getKeyCode() == KeyEvent.VK_NUMPAD0)
+				p2attack= false;
+			if(released.getKeyCode() == KeyEvent.VK_CONTROL)
+				p2tele = false;
+		}
+	}
+/**Initializes the game by loading all necessary files
+ */
+	public void init ()
+	{
+		setSize (APPLET_WIDTH, APPLET_HEIGHT);
+		background = getImage (getCodeBase(), level+".jpg");
+		music = getAudioClip (getCodeBase(), level+".au");
+		backbuffer = createImage(APPLET_WIDTH, APPLET_HEIGHT);
+      	backg = backbuffer.getGraphics();
+      	Stringmaker = (Graphics2D) backg; //helps with context
+		context = Stringmaker.getFontRenderContext(); //helps with drawP1&P2
+		backg.setFont (font);
+		Controller = new Controls();
+		GameRun = new Thread(this);
+	}
+/**Starts the game by starting the thread looping the music
+ */
+	public void start ()
+	{
+		GameRunning = true;
+		p1 = new Player(getImage (getCodeBase(), name1+"l1.png"), getImage (getCodeBase(), name1+"l2.png"),
+			getImage (getCodeBase(), name1+"r1.png"), getImage (getCodeBase(), name1+"r2.png"),
+			getImage (getCodeBase(), name1+"jl.png"), getImage (getCodeBase(), name1+"jr.png"),
+			getImage (getCodeBase(), name1+"fl.png"), getImage (getCodeBase(), name1+"fr.png"),
+			getImage (getCodeBase(), name1+"al1.png"),getImage (getCodeBase(), name1+"al2.png"),
+			getImage (getCodeBase(), name1+"ar1.png"),getImage (getCodeBase(), name1+"ar2.png"),
+			getImage (getCodeBase(), name1+"dl.png"),getImage (getCodeBase(), name1+"dr.png"),
+			getImage (getCodeBase(), name1+"die.png"),
+			name1, p1width, p1height, 1, health, p1atk, p1def, p1speed, p1range, teles);
+		p2 = new Player(getImage (getCodeBase(), name2+"l1.png"), getImage (getCodeBase(), name2+"l2.png"),
+			getImage (getCodeBase(), name2+"r1.png"), getImage (getCodeBase(), name2+"r2.png"),
+			getImage (getCodeBase(), name2+"jl.png"), getImage (getCodeBase(), name2+"jr.png"),
+			getImage (getCodeBase(), name2+"fl.png"), getImage (getCodeBase(), name2+"fr.png"),
+			getImage (getCodeBase(), name2+"al1.png"),getImage (getCodeBase(), name2+"al2.png"),
+			getImage (getCodeBase(), name2+"ar1.png"),getImage (getCodeBase(), name2+"ar2.png"),
+			getImage (getCodeBase(), name2+"dl.png"),getImage (getCodeBase(), name2+"dr.png"),
+			getImage (getCodeBase(), name2+"die.png"),
+			name2, p2width, p2height, 2, health, p2atk, p2def, p2speed, p2range, teles);
+		music.loop();
+		addKeyListener(Controller);//adds controls for game
+		GameRun.start();//starts the game
+	}
+/**Runs a game loop(thread) to for the game to occur in with a time limit
+ */
+ 	public void run ()
+ 	{
+ 		try
+ 		{
+ 			while(GameRunning)
+ 			{
+ 				update();
+ 				repaint();
+ 				GameRun.sleep(50);
+ 			}
+ 			removeKeyListener(Controller);//stop the player controls
+ 		}
+ 		catch (InterruptedException e) { }
+ 	}
+/**Handles all the game logic and movements of the characters
+ */
+	public void update ()
+	{
+		if(p1moveLeft)//allows  same time movement
+			p1.moveLeft();
+		if(p1moveRight)
+			p1.moveRight();
+		if(p1attack)
+			p1.attack(p2);
+		if(p1jump)
+			p1.jump();
+		if(p1tele)
+			p1.teleport();
+		if(p2moveLeft)
+			p2.moveLeft();
+		if(p2moveRight)
+			p2.moveRight();
+		if(p2attack)
+			p2.attack(p1);
+		if(p2jump)
+			p2.jump();
+		if(p2tele)
+			p2.teleport();
+		if(p1.isDead() && p2.isDead())//victory signs
+		{
+			victory = "You have tied.";
+			GameRunning = false;
+		}
+		else if(p1.isDead())
+		{
+			victory = name2+" has won.";
+			GameRunning = false;
+		}
+		else if(p2.isDead())
+		{
+			victory = name1+" has won.";
+			GameRunning = false;
+		}
+		p1.gravity();
+		p2.gravity();
+	}
+/**Draws the screen
+ *@param screen the screen that is drawn on
+ */
+	public void paint (Graphics screen)
+	{
+		drawLevel();
+      	drawHealth();
+      	drawPlayers();
+      	if(p1.isDead() || p2.isDead())
+      	{
+      		backg.setColor(Color.RED);
+      		backg.drawString (victory, 187, 250);
+      	}
+      	screen.drawImage(backbuffer, 0, 0, this );
+	}
+/**Draws the health
+ */
+	public void drawHealth ()
+	{
+		backg.setColor(Color.blue);
+		if(p1.isDead())
+			backg.setColor(Color.RED);
+		bounds = font.getStringBounds(name1, context); //to find where to put string
+		backg.drawString (name1, 0, (int) bounds.getHeight());
+		healthsign1 = p1.getHealth();
+		bounds2 = font.getStringBounds(healthsign1, context);
+		backg.drawString (healthsign1, 0, (int) bounds.getHeight()+ (int) bounds2.getHeight());
+		backg.setColor(Color.blue);
+		if(p2.isDead())
+			backg.setColor(Color.RED);
+		bounds = font.getStringBounds(name2, context); //to find where to put strings
+		backg.drawString (name2, APPLET_WIDTH- (int) bounds.getWidth(), (int) bounds.getHeight());
+		healthsign2 = p2.getHealth();
+		bounds2 = font.getStringBounds(healthsign2, context);
+		backg.drawString (healthsign2, APPLET_WIDTH- (int) bounds2.getWidth(), (int) bounds.getHeight()+ (int) bounds2.getHeight());
+	}
+/**Draws the players
+ */
+	public void drawPlayers ()
+	{
+		backg.drawImage (p1.getSprite(), p1.getX(), p1.getY(), p1.getWidth(), p1.getHeight(), this);
+		backg.drawImage (p2.getSprite(), p2.getX(), p2.getY(), p2.getWidth(), p2.getHeight(), this);
+	}
+/**Draws the level and plays out the effects it has on the players
+ */
+ 	public void drawLevel()
+ 	{
+		backg.drawImage(background, 0, 0, APPLET_WIDTH, APPLET_HEIGHT, this);
+ 		//more effects filled in later
+ 	}
+/**Stops the game
+ */
+	public void stop ()
+	{
+		music.stop();
+		GameRunning = false;
+	}
+}
---
+++ src/Player.java
@@ -0,0 +1,252 @@
+package libnation;
+
+import java.awt.*;
+import java.util.Random;
+/**Player.java
+ *The Representation of a Player for the Game
+ *@author David Finol 
+ *@version 1.0 12/17/06
+*/
+public class Player
+{
+	private int width, height, X, Y, player, health, atk, defense, speed, range, teles;
+	private Image sprite, right1, right2, left1, left2;
+	private Image jumpLeft, jumpRight, fallLeft, fallRight;
+	private Image attackLeft1, attackLeft2, attackRight1, attackRight2;
+	private Image damageLeft, damageRight, die;
+	private Random teleporter;
+	private boolean dead;
+/**Constructs a player with the name and stats specified
+ *@param l1 there are so many images, I won't state them all, use common sense
+ *@param l2 for example, this is the image for moving left2
+ *@param name the name of the player(mossflower, nick, etc)
+ *@param w the width size of the player
+ *@param h the height size of the player
+ *@param p the player character (player 1 or player 2)
+ *@param hp the health of the player
+ *@param at the atk stat of the player
+ *@param def the defense stat of the player
+ *@param s the movement speed stat of the player
+ *@param r the attack range stat of the player
+ *@param t the number of teleports available to the player per game
+ */
+	public Player(Image l1, Image l2, Image r1, Image r2, Image jl, Image jr, Image fl,
+			Image fr, Image al1, Image al2, Image ar1, Image ar2, Image dl, Image dr, Image dd,
+			String name, int w, int h, int p, int hp, int at, int def, int s, int r, int t)
+	{
+		//loads the paramaters, there are many
+		left1 = l1;
+		left2 = l2;
+		right1 = r1;
+		right2 = r2;
+		jumpLeft = jl;
+		jumpRight = jr;
+		fallLeft = fl;
+		fallRight = fr;
+		attackLeft1 = al1;
+		attackLeft2 = al2;
+		attackRight1 = ar1;
+		attackRight2 = ar2;
+		damageLeft = dl;
+		damageRight = dr;
+		die = dd;
+		teleporter = new Random();
+		width = w;
+		height = h;
+		player = p;
+		if(player == 1)
+		{
+			X = 0;
+			sprite = right1;
+		}
+		else
+		{
+			X = 500-width;
+			sprite = left1;
+		}
+		Y = 375-height;
+		health = hp;
+		atk = at;
+		defense = def;
+		speed = 2*s;
+		range = 70*r;
+		teles = t;
+		dead = false;
+	}
+/**Gets the current image of the player
+ *@return the image
+ */
+	public Image getSprite()
+	{
+		return sprite;
+	}
+/**Gets the current health of the player
+ *@return the health
+ */
+	public String getHealth()
+	{
+		return ""+health+"";
+	}
+/**Gets the attack stat of the player
+ *@return the atk stat
+ */
+	public int getAtk()
+	{
+		return atk;
+	}
+/**Gets the defense stat of the player
+ *@return the defense stat
+ */
+	public int getDef()
+	{
+		return defense;
+	}
+/**Gets the width size of the player image
+ *@return the width
+ */
+	public int getWidth()
+	{
+		return width;
+	}
+/**Gets the height size of the player image
+ *@return the height
+ */
+	public int getHeight()
+	{
+		return height;
+	}
+/**Gets the location of the player on the X axis
+ *@return the X-axis location
+ */
+	public int getX()
+	{
+		return X;
+	}
+/**Gets the location of the player on the Y axis
+ *@return the Y-axis location
+ */
+	public int getY()
+	{
+		return Y;
+	}
+/**Moves the player to the right
+ */
+	public void moveRight()
+	{
+		if(sprite == right1)//chooses correct image
+			sprite = right2;
+		else
+			sprite = right1;
+		if(X <= 500-getWidth())//stops from going off screend]
+			X+= speed;
+	}
+/**Moves the player to the left
+ */
+	public void moveLeft()
+	{
+		if(sprite == left1)//chooses correct animation
+			sprite = left2;
+		else
+			sprite = left1;
+		if(X >= 0)//stops from going off the screen
+			X-= speed;
+	}
+/**Makes the player jump up
+ */
+	public void jump()
+	{
+		if(sprite==left1||sprite==left2||sprite==attackLeft1||sprite==attackLeft2)
+			sprite = jumpLeft;//chooses correct animation
+		else if(sprite==fallLeft||sprite==jumpLeft||sprite==damageLeft)
+			sprite = jumpLeft;
+		else if(sprite==right1||sprite==right2||sprite==attackRight1||sprite==attackRight2)
+			sprite = jumpRight;
+		else if(sprite==fallRight||sprite==jumpRight||sprite==damageRight)
+			sprite = jumpRight;
+		if(Y >= 0)//stops form going off screen
+			Y-= 40;
+	}
+/**Keeps the character falling down
+ */
+	public void gravity()
+	{
+		if(Y <= 375-getHeight()-20)
+			Y+= 20;
+	}
+/**Teleports the player to a random spot
+ */
+	public void teleport()
+	{
+		if(teles!= 0)
+		{
+			//chooses random location to tele
+			X = teleporter.nextInt(500-getWidth());
+			teles-= 1;
+		}
+	}
+/**Heals the player
+ */
+	public void heal()
+	{
+		health+= 1;
+	}
+/**Attacks the enemy player
+ *@param the player that you are trying to attack
+ */
+	public void attack(Player enemy)
+	{
+		//chooses the correct sprite
+		if(sprite==left1||sprite==left2||sprite==attackLeft2)
+			sprite = attackLeft1;//chooses correct animation
+		else if(sprite==fallLeft||sprite==jumpLeft||sprite==damageLeft)
+			sprite = attackLeft1;
+		else if(sprite == attackLeft1)
+			sprite = attackLeft2;
+		else if(sprite==right1||sprite==right2||sprite==attackRight2)
+			sprite = attackRight1;
+		else if(sprite==fallRight||sprite==jumpRight||sprite==damageRight)
+			sprite = attackRight1;
+		else if(sprite == attackRight1)
+			sprite = attackRight2;
+		//checks to see if in range
+		for(int check=0; check<=range; check++)
+			if(getX()==enemy.getX()+(range-check) || getX()==enemy.getX()-(range-check))
+				for(int y=0;y<=enemy.getHeight();y++)
+				 	if(getY()==enemy.getY()+(enemy.getHeight()-y) || getY()==enemy.getY()-(enemy.getHeight()-y)) 
+						enemy.damage(this);
+	}
+/**Loses health for the player when attacked
+ *@param the player who attacked you
+ */
+	private void damage(Player attacker)
+	{
+		if(sprite==left1||sprite==left2||sprite==attackLeft1||sprite==attackLeft2)
+			sprite = damageLeft;//chooses correct animation
+		else if(sprite==fallLeft||sprite==jumpLeft||sprite==damageLeft)
+			sprite = damageLeft;
+		else if(sprite==right1||sprite==right2||sprite==attackRight1||sprite==attackRight2)
+			sprite = damageRight;
+		else if(sprite==fallRight||sprite==jumpRight||sprite==damageRight)
+			sprite = damageRight;
+		health-= attacker.getAtk();//needs to be modified
+		if(health <= 0)//checks to see if dead
+		{
+			health = 0;
+			die();
+		}
+	}
+/**Causes the player to use death sprite
+ */
+	private void die()
+	{
+		sprite = die;
+		dead = true;
+	}
+/**Checks to see if the player is dead
+ *@return the death status of the player
+ */
+	public boolean isDead()
+	{
+		return dead;
+	}
+}
